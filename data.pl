98dacb89223aee2ed4bedab7a6fe827cbc4031bf
Gabriela Ossowska <gab.ossowska@gmail.com>
2019-01-05 13:25:38 +0100

    remove i choose_best ??????

--- data.pl
+++ data.pl
@@ -1,8 +1,9 @@
-variables([a,b]).
+variables([a,b,c]).
 :- dynamic variables/1 .
 
-known_fact(matka(M,A)).
-known_fact(brat(A,B)).
+known_fact(matka(grazyna,seba)).
+known_fact(brat(seba,karyna)).
+known_fact(matka(grazyna,karyna)).
 
 predicate(matka, 2).
 predicate(brat, 2).
